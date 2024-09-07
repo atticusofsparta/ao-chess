90914cf72d90dbb61aeedd219e595b5aceee9e55
atticusofsparta <atticusofsparta@protonmail.com>
2024-09-07 15:27:30 -0600

    update aos bundle util to accept a path argument for entry file

--- processes/tools/bundle-aos.js
+++ processes/tools/bundle-aos.js
@@ -5,17 +5,37 @@ import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 async function main() {
-  console.log('Bundling Lua...');
-  const pathToLua = path.join(__dirname, '../src/aos.lua');
-  console.log('Path to Lua:', pathToLua);
-  const bundledLua = bundle(path.join(__dirname, '../src/aos.lua'));
-
-  if (!fs.existsSync(path.join(__dirname, '../dist'))) {
-    fs.mkdirSync(path.join(__dirname, '../dist'));
+  const args = process.argv.slice(2); // Get CLI arguments
+  if (args.length === 0) {
+    console.error('Please provide a path name as a CLI argument.');
+    return;
   }
 
-  fs.writeFileSync(path.join(__dirname, '../dist/aos-bundled.lua'), bundledLua);
-  console.log('Doth Lua hath been bundled!');
+  let pathToLua;
+  for (let i = 0; i < args.length; i++) {
+    if (args[i].startsWith('--path=')) {
+      pathToLua = args[i].substring(7);
+      break;
+    }
+  }
+
+  if (!pathToLua) {
+    console.error('Please provide a valid --path argument.');
+    return;
+  }
+
+  console.log('Path to Lua:', pathToLua);
+  const bundledLua = bundle(pathToLua);
+
+  const entryDir = path.dirname(pathToLua); // Get the directory of the entry file
+  const distDir = path.join(entryDir, 'dist'); // Create the dist directory in the entry file's directory
+
+  if (!fs.existsSync(distDir)) {
+    fs.mkdirSync(distDir);
+  }
+
+  fs.writeFileSync(path.join(distDir, 'aos-bundled.lua'), bundledLua);
+  console.log('Lua has been bundled!');
 }
 
 main();
